b04713bb0f1e89c65b5a1fcb88e1cf51c23b6350
asturur <andreabogazzi79@gmail.com>
2024-02-21 13:18:38 +0100

    progress

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -287,6 +287,7 @@ func StartDaemon(cfg *config.UserConfig) (func() error, error) {
 			}
 
 			activeCard := state.GetActiveCard()
+			activeCoreName := mister.GetActiveCoreName()
 			newScanned, removed, err := pollDevice(cfg, &pnd, activeCard, ttp, pbp)
 			if errors.Is(err, nfc.Error(nfc.EIO)) {
 				log.Error().Msgf("error during poll: %s", err)
@@ -306,6 +307,8 @@ func StartDaemon(cfg *config.UserConfig) (func() error, error) {
 
 			state.SetActiveCard(newScanned)
 
+			// to do check string functions in go to check if
+			// activeCoreName is in cfg.TapTo.ExitGame
 			if removed && cfg.TapTo.ExitGame && !state.IsLauncherDisabled() {
 				mister.ExitGame()
 				goto end
