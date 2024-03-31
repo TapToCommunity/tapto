55f2e429e26ed41c59f1452595fc1f60fc681239
Callan Barrett <callan@zoocar.org>
2024-03-31 08:39:10 +0800

    http middleware

--- pkg/daemon/api/api.go
+++ pkg/daemon/api/api.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"runtime/debug"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 	"github.com/go-chi/render"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/wizzomafizzo/tapto/pkg/config"
 	"github.com/wizzomafizzo/tapto/pkg/daemon/api/websocket"
@@ -60,6 +62,53 @@ func setupWs(st *state.State, tr *mister.Tracker) {
 	log.Logger = log.Output(io.MultiWriter(writers...))
 }
 
+// https://github.com/ironstar-io/chizerolog/blob/master/main.go
+func LoggerMiddleware(logger *zerolog.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := func(w http.ResponseWriter, r *http.Request) {
+			log := logger.With().Logger()
+
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+
+			t1 := time.Now()
+			defer func() {
+				t2 := time.Now()
+
+				// Recover and record stack traces in case of a panic
+				if rec := recover(); rec != nil {
+					log.Error().
+						Str("type", "error").
+						Timestamp().
+						Interface("recover_info", rec).
+						Bytes("debug_stack", debug.Stack()).
+						Msg("log system error")
+					http.Error(ww, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				}
+
+				// log end request
+				log.Info().
+					Str("type", "access").
+					Timestamp().
+					Fields(map[string]interface{}{
+						"remote_ip":  r.RemoteAddr,
+						"url":        r.URL.Path,
+						"proto":      r.Proto,
+						"method":     r.Method,
+						"user_agent": r.Header.Get("User-Agent"),
+						"status":     ww.Status(),
+						"latency_ms": float64(t2.Sub(t1).Nanoseconds()) / 1000000.0,
+						"bytes_in":   r.Header.Get("Content-Length"),
+						"bytes_out":  ww.BytesWritten(),
+					}).
+					Msg("incoming_request")
+			}()
+
+			next.ServeHTTP(ww, r)
+		}
+		return http.HandlerFunc(fn)
+	}
+}
+
 func RunApiServer(
 	cfg *config.UserConfig,
 	st *state.State,
@@ -69,14 +118,16 @@ func RunApiServer(
 ) {
 	r := chi.NewRouter()
 
+	r.Use(middleware.RealIP)
+	r.Use(LoggerMiddleware(&log.Logger))
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedOrigins:   []string{"https://*", "http://*", "capacitor://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: false,
-		MaxAge:           300,
+		AllowCredentials: true,
 	}))
 
 	r.Route("/api/v1", func(r chi.Router) {
@@ -101,14 +152,18 @@ func RunApiServer(
 
 		// PUT /settings
 		r.Get("/settings", handleSettings(cfg))
-		r.Get("/settings/log", handleSettingsDownloadLog())
+		r.Get("/settings/log/download", handleSettingsDownloadLog())
 		r.Post("/settings/index/games", handleIndexGames(cfg))
 	})
 
 	setupWs(st, tr)
 	r.HandleFunc("/api/v1/ws", websocket.Handle(
 		func() []string {
-			return []string{}
+			status, err := json.Marshal(newStatus(st, tr))
+			if err != nil {
+				log.Error().Err(err).Msg("error encoding status")
+			}
+			return []string{"STATUS " + string(status)}
 		},
 		func(msg string) string {
 			return ""
