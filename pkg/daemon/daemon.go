7a21dc36f12f60986cb16fef44aa61f4b5bbd12a
Callan Barrett <callan@zoocar.org>
2024-04-11 15:10:20 +0800

    fix nfc.csv

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -71,6 +71,12 @@ func launchCard(cfg *config.UserConfig, state *state.State, db *database.Databas
 	text := card.Text
 	override := false
 
+	if v, ok := uidMap[card.UID]; ok {
+		log.Info().Msg("launching with csv uid match override")
+		text = v
+		override = true
+	}
+
 	if v, err := db.GetUidMapping(card.UID); err == nil {
 		if err != nil {
 			log.Error().Err(err).Msgf("error getting db uid mapping")
@@ -79,21 +85,9 @@ func launchCard(cfg *config.UserConfig, state *state.State, db *database.Databas
 			text = v
 			override = true
 		}
-	} else if v, ok := uidMap[card.UID]; ok {
-		log.Info().Msg("launching with csv uid match override")
-		text = v
-		override = true
 	}
 
-	if v, err := db.GetTextMapping(card.Text); err == nil {
-		if err != nil {
-			log.Error().Err(err).Msgf("error getting db text mapping")
-		} else if v != "" {
-			log.Info().Msg("launching with db text match override")
-			text = v
-			override = true
-		}
-	} else if v, ok := textMap[card.Text]; ok {
+	if v, ok := textMap[card.Text]; ok {
 		log.Info().Msg("launching with csv text match override")
 		text = v
 		override = true
