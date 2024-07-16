1e202878c7167e1315b4d916649cd1820e08a5df
Callan Barrett <callan@zoocar.org>
2024-07-16 23:25:04 +0800

    test batocera platform

---
+++ cmd/batocera/main.go
@@ -0,0 +1,104 @@
+//go:build linux
+
+/*
+TapTo
+Copyright (C) 2023 Gareth Jones
+Copyright (C) 2023, 2024 Callan Barrett
+
+This file is part of TapTo.
+
+TapTo is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+TapTo is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with TapTo.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/wizzomafizzo/tapto/pkg/platforms/batocera"
+	"github.com/wizzomafizzo/tapto/pkg/utils"
+
+	"github.com/wizzomafizzo/tapto/pkg/config"
+	"github.com/wizzomafizzo/tapto/pkg/daemon"
+)
+
+const appName = "tapto"
+
+func main() {
+	versionOpt := flag.Bool("version", false, "print version and exit")
+	flag.Parse()
+
+	if *versionOpt {
+		fmt.Println("TapTo v" + config.Version + " (mistex)")
+		os.Exit(0)
+	}
+
+	pl := &batocera.Platform{}
+	err := utils.InitLogging(pl)
+	if err != nil {
+		fmt.Println("Error initializing logging:", err)
+		os.Exit(1)
+	}
+
+	cfg, err := config.NewUserConfig(appName, &config.UserConfig{
+		TapTo: config.TapToConfig{
+			ProbeDevice: true,
+			ApiPort:     config.DefaultApiPort,
+		},
+	})
+	if err != nil {
+		log.Error().Msgf("error loading user config: %s", err)
+		fmt.Println("Error loading config:", err)
+		os.Exit(1)
+	}
+
+	if cfg.GetDebug() {
+		zerolog.SetGlobalLevel(zerolog.DebugLevel)
+	} else {
+		zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	}
+
+	fmt.Println("TapTo v" + config.Version)
+
+	stopSvc, err := daemon.StartDaemon(pl, cfg)
+	if err != nil {
+		log.Error().Msgf("error starting service: %s", err)
+		fmt.Println("Error starting service:", err)
+		os.Exit(1)
+	}
+
+	ip, err := utils.GetLocalIp()
+	if err != nil {
+		fmt.Println("Device address: Unknown")
+	} else {
+		fmt.Println("Device address:", ip.String())
+	}
+
+	fmt.Println("Press Enter to exit")
+	fmt.Scanln()
+
+	err = stopSvc()
+	if err != nil {
+		log.Error().Msgf("error stopping service: %s", err)
+		fmt.Println("Error stopping service:", err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
---
+++ pkg/platforms/batocera/platform.go
@@ -0,0 +1,167 @@
+package batocera
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/rs/zerolog/log"
+	"github.com/wizzomafizzo/tapto/pkg/config"
+	"github.com/wizzomafizzo/tapto/pkg/platforms"
+	"github.com/wizzomafizzo/tapto/pkg/readers"
+	"github.com/wizzomafizzo/tapto/pkg/readers/file"
+	"github.com/wizzomafizzo/tapto/pkg/readers/libnfc"
+	"github.com/wizzomafizzo/tapto/pkg/readers/simple_serial"
+	"github.com/wizzomafizzo/tapto/pkg/tokens"
+)
+
+type Platform struct {
+}
+
+func (p *Platform) Id() string {
+	return "batocera"
+}
+
+func (p *Platform) SupportedReaders(cfg *config.UserConfig) []readers.Reader {
+	return []readers.Reader{
+		libnfc.NewReader(cfg),
+		file.NewReader(cfg),
+		simple_serial.NewReader(cfg),
+	}
+}
+
+func (p *Platform) Setup(cfg *config.UserConfig) error {
+	return nil
+}
+
+func (p *Platform) Stop() error {
+	return nil
+}
+
+func (p *Platform) AfterScanHook(token tokens.Token) error {
+	return nil
+}
+
+func (p *Platform) ReadersUpdateHook(readers map[string]*readers.Reader) error {
+	return nil
+}
+
+func (p *Platform) RootFolders(cfg *config.UserConfig) []string {
+	return []string{
+		"C:\\scratch",
+	}
+}
+
+func (p *Platform) ZipsAsFolders() bool {
+	return false
+}
+
+func exeDir() string {
+	exe, err := os.Executable()
+	if err != nil {
+		return ""
+	}
+
+	return filepath.Dir(exe)
+}
+
+func (p *Platform) ConfigFolder() string {
+	return filepath.Join(exeDir(), "data")
+}
+
+func (p *Platform) LogFolder() string {
+	return filepath.Join(exeDir(), "logs")
+}
+
+func (p *Platform) NormalizePath(cfg *config.UserConfig, path string) string {
+	return path
+}
+
+func LaunchMenu() error {
+	return nil
+}
+
+func (p *Platform) KillLauncher() error {
+	return nil
+}
+
+func (p *Platform) LaunchingEnabled() bool {
+	return true
+}
+
+func (p *Platform) SetLaunching(disabled bool) error {
+	return nil
+}
+
+func (p *Platform) GetActiveLauncher() string {
+	return ""
+}
+
+func (p *Platform) PlayFailSound(cfg *config.UserConfig) {
+}
+
+func (p *Platform) PlaySuccessSound(cfg *config.UserConfig) {
+}
+
+func (p *Platform) ActiveSystem() string {
+	return ""
+}
+
+func (p *Platform) ActiveGame() string {
+	return ""
+}
+
+func (p *Platform) ActiveGameName() string {
+	return ""
+}
+
+func (p *Platform) ActiveGamePath() string {
+	return ""
+}
+
+func (p *Platform) SetEventHook(f *func()) {
+}
+
+func (p *Platform) LaunchSystem(cfg *config.UserConfig, id string) error {
+	log.Info().Msgf("launching system: %s", id)
+	return nil
+}
+
+func (p *Platform) LaunchFile(cfg *config.UserConfig, path string) error {
+	log.Info().Msgf("launching file: %s", path)
+
+	if filepath.Ext(path) == ".txt" {
+		// get filename minus ext
+
+		fn := filepath.Base(path)
+		fn = fn[:len(fn)-4]
+
+		return exec.Command("cmd", "/c", "C:\\Program Files (x86)\\Steam\\steam.exe", "steam://rungameid/"+fn).Start()
+	}
+
+	return nil
+}
+
+func (p *Platform) Shell(cmd string) error {
+	return nil
+}
+
+func (p *Platform) KeyboardInput(input string) error {
+	return nil
+}
+
+func (p *Platform) KeyboardPress(name string) error {
+	return nil
+}
+
+func (p *Platform) GamepadPress(name string) error {
+	return nil
+}
+
+func (p *Platform) ForwardCmd(env platforms.CmdEnv) error {
+	return nil
+}
+
+func (p *Platform) LookupMapping(_ tokens.Token) (string, bool) {
+	return "", false
+}
